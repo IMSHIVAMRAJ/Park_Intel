3caad627c0e35947c330d24d88bcf56850b2eb79
IMSHIVAMRAJ <sraj2004bxr@gmail.com>
2026-02-25 18:07:16 +0530

    docker_added and deployed sql and redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,7 +1,19 @@
+// const { createClient } = require("redis");
+
+// const redisClient = createClient({
+//   url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+// });
+
+// redisClient.connect()
+//   .then(() => console.log("🔥 Redis connected"))
+//   .catch(console.error);
+
+// module.exports = redisClient;
+
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: process.env.REDIS_URL
 });
 
 redisClient.connect()
